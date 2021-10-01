50ebaf3d31cd380ed22f09de5d6e7a98adcae9bb
Andre Velasquez <andrethefirst21@gmail.com>
2021-10-01 15:27:54 -0700

    pass Order recieved test

--- PierresTracker/Models/Order.cs
+++ PierresTracker/Models/Order.cs
@@ -4,6 +4,36 @@ namespace PierresTracker.Models
 {
   public class Order
   {
+    public string Description { get; set; }
+    public string Title { get; set; }
+    public string Date { get; set; }
+    public string Price { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
+
+    public Order(string title, string description, string date, string price)
+    {
+      Title = title;
+      Description = description;
+      Date = date;
+      Price = price;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear()
+    }
     
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1]; //index placement
+    }
   }
 }
